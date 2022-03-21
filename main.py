4816e9cdcd7a50e127d40f7b4dfd3cc47e915822
ChetanKumar-Sura <81322260+chetankumar-sura@users.noreply.github.com>
2022-03-21 10:10:11 +0000

    adding one example to it

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import my_random1
 random_integer = random.randint(1,7)
 print(random_integer)
 
-
-"""ramndom float values"""
+#ramndom float values
 random_float = random.rand() * 5
 print(random_float)
+#random love score
+love_score = random.randint(50,100)
+print(f"your love score is {love_score}")
